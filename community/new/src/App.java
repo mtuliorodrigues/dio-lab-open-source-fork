351276c6cf36cdfd1d9c6b8878b879c1b6fe9fb8
mtuliorodrigues <marcutulio.ttulio@gmail.com>
2024-06-05 21:46:37 -0300

    novas atualizações

---
+++ community/new/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        boolean condicao = false;
+
+        while(condicao){
+            System.out.println("executou ... ");
+        }
+    }
+
+}
